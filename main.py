09b176c305f690d8996c6d6a4d7bbf217e522d87
sutaroh <joejet555@gmail.com>
2022-10-08 10:03:38 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import time
+
+st.title('Streamlit 超入門')
+
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.enpty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0,1)
+
+'Done!!!!!'
+
+left_column, right_column = st.beta_columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右コラム')
+
+expander1 = st.beta_expander('問い合わせ１')
+expander1.write('問い合わせ１の回答')
+expander2 = st.beta_expander('問い合わせ２')
+expander2.write('問い合わせ２の回答')
+expander3 = st.beta_expander('問い合わせ３')
+expander3.write('問い合わせ３の回答')
+
